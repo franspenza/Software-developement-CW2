f0e71d9a08e700cc3eed3e82aafc7e33743e3b00
Christian Attard <chris.att.91@gmail.com>
2022-07-18 12:08:55 +0200

    Created ScheduleEvent class, still in progress

--- EventPlanner-CW2/src/main.java
+++ EventPlanner-CW2/src/main.java
@@ -1,8 +1,12 @@
+import javax.swing.JFrame;
 
 public class main {
 
 	public static void main(String[] args) {
-
+		
+		ScheduleEvent scheduleEvent = new ScheduleEvent();
+		scheduleEvent.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        scheduleEvent.setVisible(true);
 	}
 
 }
