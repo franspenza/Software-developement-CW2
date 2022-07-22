5688e63e9c254d8d13bab7b3a96434c40146ac1b
Christian Attard <chris.att.91@gmail.com>
2022-07-22 18:34:29 +0200

    Added comments in UserDialog class, Add event to calendar GUI

--- EventPlanner-CW2/src/UserDialog.java
+++ EventPlanner-CW2/src/UserDialog.java
@@ -9,7 +9,10 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+//Add event GUI
 public class UserDialog extends JDialog {
+	
+	//declaring label, textfields, EventDateModel and LocalDate 
 	private JLabel dayLabel = new JLabel("Day field here");
 	private JTextField startField = new JTextField("00:00am");
 	private JTextField endField = new JTextField("00:00am");
@@ -17,28 +20,39 @@ public class UserDialog extends JDialog {
 	private EventDateModel model;
 	private LocalDate date;
 	
+	//constructor referring to parent class constructor JDialog  
 	public UserDialog(JFrame owner){
-		super(owner, "Create Event", true);
+		super(owner, "Create Event", true);		
+		//creating new BorderLayout
 		this.add(eventField, BorderLayout.NORTH);
+		//creating new JPanel to add label, and text fields
 		JPanel panel = new JPanel();
 		panel.add(dayLabel);
 		panel.add(startField);
 		panel.add(endField);
 		
+		//creating save button and calling the ActionListener Java library class
 		JButton save = new JButton("Save");
 		save.addActionListener(event->{
+			//temporary store user input start time and end time, to EventSchedule.Time start and end
 			EventSchedule.Time start = extractTime(startField.getText());
 			EventSchedule.Time end = extractTime(endField.getText());
 			System.out.println(start);
 	    	System.out.println(end);
+	    	//temporary store all user inputs as a new event calling the constructor of EventSchedule
 			EventSchedule sampleEvent = new EventSchedule(eventField.getText(), start,end);
 			
+			//a check to determine if the event will clash with other already stored events
+			//calling addEventToDate method
 			boolean addResult = model.addEventToDate(date, sampleEvent);
+			//if there is no clash, the listener will update the events for that current day
 			if(addResult){
 				setVisible(false);
+				//calling the update method from EventDateModel class
 				model.update();
 			}
 			else{
+				//a conflict message will display
 				JOptionPane.showMessageDialog(this, "The time for this event conflicts with an "
 						+ "existing event");
 				setVisible(true);
@@ -51,12 +65,14 @@ public class UserDialog extends JDialog {
 		
 	}
 	
+	//method to display today's date in label dayLabel
 	public void attachModel(EventDateModel model, LocalDate date){
 		this.model = model;
 		this.date = date;
 		dayLabel.setText(date.toString());
 	}
 	
+	//Extracting user input method
 	public EventSchedule.Time extractTime(String line){
 		int colon = line.indexOf(":");
 		int hour = Integer.parseInt(line.substring(0, colon));
